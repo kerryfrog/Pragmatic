0980ef29361e98281fd6fce24e8dd3e48f69d239
kerryfrog <idasom060@gmail.com>
2021-07-30 20:16:11 +0900

    django course 41

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,11 +8,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 from django.urls import reverse, reverse_lazy
+
+from articleapp.models import Article
 
 has_ownership = [ account_ownership_required , login_required]
 
@@ -39,10 +42,16 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')  #reverse_lazy 클래스에서 사용하려면 reverse_lazy 사용해야함
     template_name = 'accountapp/create.html'   #어떤 html 파일을 이용해서 볼것인지
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 #일반 function에서 사용하는걸 메서드에서도 사용할 수 있게 해주는 데코레이터
 @method_decorator(has_ownership, 'get')
